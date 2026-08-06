264e14293bb8534bd79445f1a237a66fc49c1357
emmanuelkings963-lgtm <emmanuelkings963@gmail.com>
2026-08-07 00:47:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 let enemyHp = 100;
-let xp = 0;
-let level = 1;
+
 function attack() {
-    enemyHp = enemyHp - 10;
-
-    if (enemyHp < 0) {
-        enemyHp = 0;
-    }
-
-    document.getElementById("enemyHp").innerHTML = enemyHp;
-
-    if (enemyHp === 0) {
-    xp = xp + 50;
-    document.getElementById("xpText").innerHTML = xp;
-    alert("🏆 Enemy Defeated!\n⭐ +50 XP");
-    }
-enemyHp = 100;
-document.getElementById("enemyHp").innerHTML = enemyHp;
+    alert("Attack button works!");
+}
