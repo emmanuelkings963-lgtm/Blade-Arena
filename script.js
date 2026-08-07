01c799862dc7a2225b6f1d985c2b2446a8d67634
emmanuelkings963-lgtm <emmanuelkings963@gmail.com>
2026-08-07 01:55:35 +0100

    Add enemy hit effect with timeout

--- script.js
+++ script.js
@@ -9,6 +9,13 @@ function attack() {
 
     document.getElementById("enemyHp").textContent = enemyHp;
 document.getElementById("hpBar").style.width = enemyHp + "%";
+   let enemy = document.querySelector(".enemy");
+
+enemy.classList.add("enemy-hit");
+
+setTimeout(() => {
+    enemy.classList.remove("enemy-hit");
+}, 300);
     if (enemyHp === 0) {
         alert("🏆 Enemy Defeated!");
 document.querySelector(".enemy").style.display = "none";
